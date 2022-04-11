e993843e4ec277c0b9ce02140ac4a813673aa1e3
alexresdev <alexresdev@gmail.com>
2022-04-11 14:39:32 +0300

    Directives

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-    collapsed = true;
-}
+    @Output() feautureSelected = new EventEmitter<string>();
+    
+    onSelect(feature: string) {
+        this.feautureSelected.emit(feature);
+    }
+ }
--- client/src/app/recipes/recipe-list/recipe-list.component.ts
+++ client/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simple a test', 
     'https://cdn.pixabay.com/photo/2021/12/30/11/33/italian-cuisine-6903774_960_720.jpg'),
@@ -17,6 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
