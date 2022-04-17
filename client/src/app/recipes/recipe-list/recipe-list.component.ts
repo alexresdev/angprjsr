80b02e880838b4309e9dd1471cd2a9d78a29ba6b
alexresdev <alexresdev@gmail.com>
2022-04-17 17:32:06 +0300

    Services and DI

--- client/src/app/recipes/recipe-list/recipe-list.component.ts
+++ client/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,21 +9,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simple a test', 
-    'https://cdn.pixabay.com/photo/2021/12/30/11/33/italian-cuisine-6903774_960_720.jpg'),
-    new Recipe('A Test Recipe', 'This is simple a test', 
-    'https://cdn.pixabay.com/photo/2021/12/30/11/33/italian-cuisine-6903774_960_720.jpg'),
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private recipeService: RecipeService ) {
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
---
+++ client/src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Chicken',
+            'Perfect Roast Chicken', 
+            'https://cdn.pixabay.com/photo/2021/08/15/07/56/chicken-6547126_960_720.png',
+            [
+                new Ingredient('Chicken', 1),
+                new Ingredient('Spices', 20)
+            ]),
+        new Recipe(
+            'Burger',
+            'The best Burger have you ever eaten', 
+            'https://cdn.pixabay.com/photo/2014/12/21/23/56/hamburger-576419_960_720.png',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ]),
+      ];
+
+      constructor(private slService: ShoppingListService) {
+      }
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+          this.slService.addIngredients(ingredients);
+      }
+}
