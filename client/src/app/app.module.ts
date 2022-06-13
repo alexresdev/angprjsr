20e849a814256607bf6463b695fad56a76505ee0
alexresdev <alexresdev@gmail.com>
2022-06-13 13:13:57 +0300

    Http

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,7 +13,6 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
@@ -35,7 +36,7 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    //HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService],
--- client/src/app/recipes/recipe.service.ts
+++ client/src/app/recipes/recipe.service.ts
@@ -10,52 +10,59 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Chicken',
-            'Perfect Roast Chicken', 
-            'https://pigmine.ru/wp-content/uploads/0/6/7/06761109ae87b93097786de8b0f92c67.jpeg',
-            [
-                new Ingredient('Chicken', 1),
-                new Ingredient('Spices', 20)
-            ]),
-        new Recipe(
-            'Burger',
-            'The best Burger have you ever eaten', 
-            'https://farsh-recept.ru/wp-content/uploads/2020/04/s-bekonom.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ]),
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Chicken',
+    //         'Perfect Roast Chicken', 
+    //         'https://pigmine.ru/wp-content/uploads/0/6/7/06761109ae87b93097786de8b0f92c67.jpeg',
+    //         [
+    //             new Ingredient('Chicken', 1),
+    //             new Ingredient('Spices', 20)
+    //         ]),
+    //     new Recipe(
+    //         'Burger',
+    //         'The best Burger have you ever eaten', 
+    //         'https://farsh-recept.ru/wp-content/uploads/2020/04/s-bekonom.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ]),
+    //   ];
 
-      constructor(private slService: ShoppingListService) {
-      }
+    private recipes: Recipe[] = [];
 
-      getRecipes() {
-          return this.recipes.slice();
-      }
+    constructor(private slService: ShoppingListService) {
+    }
 
-      getRecipe(index: number) {
-          return this.recipes[index];
-      }
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
-      addIngredientsToShoppingList(ingredients: Ingredient[]) {
-          this.slService.addIngredients(ingredients);
-      }
+    getRecipes() {
+        return this.recipes.slice();
+    }
 
-      addRecipe(recipe: Recipe) {
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
         this.recipes.push(recipe);
         this.recipesChanged.next(this.recipes.slice());
-      }
+    }
 
-      updateRecipe(index: number, newRecipe: Recipe) {
+    updateRecipe(index: number, newRecipe: Recipe) {
         this.recipes[index] = newRecipe;
         this.recipesChanged.next(this.recipes.slice());
-      }
+    }
 
-      deleteRecipe(index: number) {
+    deleteRecipe(index: number) {
         this.recipes.splice(index, 1);
         this.recipesChanged.next(this.recipes.slice());
-      }
+    }
 }
